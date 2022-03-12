4b6b71fcd7bbfeee3ece42343e205a29fe95eca3
Masterpiece513 <imaobongud@gmail.com>
2022-03-12 13:27:38 -0800

    print the smallest possible combination of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: print only the smallest possible combinations of two digit numbers
+ * numbers must be seperated by a comma and a space
+ * Return: 0
+ */
+int main(void)
+{
+	int a = 0;
+	int b, c;
+
+	while (a < 100)
+	{
+		b = a % 10;
+		c = a / 10;
+		
+		if (c < b)
+		{
+			putchar(c + '0');
+			putchar(b + '0');
+
+			if (a < 89)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+
+		a++
+	}
+	putchar('\n');
+
+	return (0);
+}
