4aba1e259913bbe3f02d1b510a9c45ff5e1daf7d
Ima <98466210+Masterpiece513@users.noreply.github.com>
2022-03-15 20:20:42 +0100

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,13 +10,11 @@ void times_table(void)
 	for (num = 0; num <= 9; num++)
 	{
 		_putchar('0');
-	}
 	for (mul = 1; mul <= 9; mul++)
 	{
 		_putchar(44);
 		_putchar(32);
 		result = num * mul;
-	}
 	if ((result / 10) > 0)
 	{
 		_putchar((result / 10) + '0');
@@ -30,6 +28,7 @@ void times_table(void)
 	{
 		_putchar(44);
 	        _putchar(32);
+	}
 	}
 	_putchar('\n');
 	}
