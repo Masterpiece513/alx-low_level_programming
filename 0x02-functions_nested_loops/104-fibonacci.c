0a3c8d8e26142b035aa3e3da9cc982581550fd49
Ima <98466210+Masterpiece513@users.noreply.github.com>
2022-03-16 18:40:33 +0100

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,16 +9,15 @@ int main(void)
 	long int num1 = 1, num2 = 2;
 	long int next_num = 0;
 
-	printf("%ld, ", num1);
 	while (i <= 98)
 	{
 		if (i == 98)
 		{
-			printf("%ld\n", next_num);
+			printf("%ld\n", num1);
 		}
 		else
 		{
-			printf("%ld, ", next_num);
+			printf("%ld, ", num1);
 		}
 		next_num = num1 + num2;
 		num1 = num2;
