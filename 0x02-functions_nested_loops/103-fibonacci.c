39b2e50f98a3c6bf869cfd158cb0b5caa98685fa
Masterpiece513 <imaobongud@gmail.com>
2022-03-16 09:36:16 -0700

    print the sum of even terms in fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,20 @@
+#include <stdio>
+/**
+ * main - print the sum of even Fibonacci numbers
+ * up to 4000000
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 1, j = 2, k = 0, sum = 0;
+	while (j <= 4000000)
+	{
+		if ((j % 2) == 0)
+			sum += j;
+		k = i + j;
+		i = j;
+		j = k;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}	
