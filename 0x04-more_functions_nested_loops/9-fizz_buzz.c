d9e6c131c427a44511a9e9f03bedc684e8948d5c
Ima <98466210+Masterpiece513@users.noreply.github.com>
2022-03-17 19:03:22 +0100

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -22,16 +22,15 @@ int main(void)
 			}
 			else if (i % 5 == 0)
 			{
-				printf("Buzz ");
+				for (i == 100)
+					printf("Buzz");
+				for (i != 100)
+					printf("Buzz ");
 			}
 			else
 			{
 				printf("%i ", i);
 			}
-		}
-		if (i == 100)
-		{
-			printf("Buzz");
 		}	
 	}
 	printf("\n");
