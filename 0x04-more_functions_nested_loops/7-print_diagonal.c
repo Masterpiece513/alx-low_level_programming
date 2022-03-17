04693edc6fab2819d24416c2feeafc84ccaf3781
Ima <98466210+Masterpiece513@users.noreply.github.com>
2022-03-17 17:27:32 +0100

    draw a diagonal in the terminal
    using the '\' character

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -5,19 +5,22 @@
  */
 void print_diagonal(int n)
 {
-	int count = 0, space;
+	int count, space;
 
 	if (n > 0)
 	{
-		while (count < n)
+		for (count = 0; count < n; count++)
 		{
 			for (space = 0; space < count; space++)
 			{
 				_putchar(' ');
 			}
 			_putchar('\\');
+			if (count == n-1)
+			{
+				continue;
+			}
 			_putchar('\n');
-			count++;
 		}
 	}
 	else
