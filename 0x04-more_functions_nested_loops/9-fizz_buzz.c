fd537ca2f9c9ef3d3a1f7723e32cfac1639f0d06
Ima <98466210+Masterpiece513@users.noreply.github.com>
2022-03-17 18:05:09 +0100

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,15 +10,15 @@ int main(void)
 
 	for (i = 1; i <= 99; i++)
 	{
-		if ((i % 3) == 0 && (i % 5) == 0)
+		if (i % 15 == 0)
 		{
 			printf("FizzBuzz ");
 		}
-		else if ((i % 3) == 0)
+		else if (i % 3 == 0)
 		{
 			printf("Fizz ");
 		}
-		else if ((i % 5) == 0)
+		else if (i % 5 == 0)
 		{
 			printf("Buzz ");
 		}
@@ -27,6 +27,6 @@ int main(void)
 			printf("%i ", i);
 		}
 	}
-	printf('Buzz\n');
+	printf("Buzz\n");
 	return (0);
 }
