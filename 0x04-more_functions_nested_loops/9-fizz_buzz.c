2913496efebb0ce2a0ea876d9ade4a2692fc6b89
Ima <98466210+Masterpiece513@users.noreply.github.com>
2022-03-17 18:11:09 +0100

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,27 +6,28 @@
  */
 int main(void)
 {
-	int i;
+	int num;
 
-	for (i = 1; i <= 99; i++)
+	for (num = 1; num <= 100; num++)
 	{
-		if (i % 15 == 0)
-		{
-			printf("FizzBuzz ");
-		}
-		else if (i % 3 == 0)
-		{
-			printf("Fizz ");
-		}
-		else if (i % 5 == 0)
-		{
-			printf("Buzz ");
-		}
+		if ((num % 3) == 0 && (num % 5) == 0)
+			printf("FizzBuzz");
+
+		else if ((num % 3) == 0)
+			printf("Fizz");
+
+		else if ((num % 5) == 0)
+			printf("Buzz");
+
 		else
-		{
-			printf("%i ", i);
-		}
+			printf("%d", num);
+
+		if (num == 100)
+			continue;
+		printf(" ");
 	}
-	printf("Buzz\n");
+
+	printf("\n");
+
 	return (0);
 }
