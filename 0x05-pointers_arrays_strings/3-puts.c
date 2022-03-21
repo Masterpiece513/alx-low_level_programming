7648e544d0823aac3e5644b00a7d8beacbd26052
Masterpiece513 <imaobongud@gmail.com>
2022-03-21 12:28:30 -0700

    prints a string followed by a newline

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts - prints string
+ * @str: input string
+ * Return: 0
+ */
+void _puts(char *s)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar(10);
+}
