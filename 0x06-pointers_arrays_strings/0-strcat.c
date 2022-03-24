33bfe416ef019425def9170006b334af1df306c2
Ima <98466210+Masterpiece513@users.noreply.github.com>
2022-03-24 10:54:10 +0100

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,15 +5,16 @@
  * @src: pointer to string
  * Return: dest
  */
-char *_strncpy(char *dest, char *src, int n)
+char *_strcat(char *dest, char *src)
 {
-	int i;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[i] = src[i];
+	int index = 0, dest_len = 0;
 
-	for ( ; i < n; i++)
-		dest[i] = '\0';
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
 
 	return (dest);
 }
