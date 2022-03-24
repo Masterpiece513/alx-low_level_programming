411cb82403d9b728dddb1296a0c53cccc1881ab0
Ima <98466210+Masterpiece513@users.noreply.github.com>
2022-03-24 13:02:31 +0100

    changes all lowercase of a string to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
 *string_toupper - changes all lowercases letters of a string to uppercase
-*@str: input string
+*@s: input string
 *Return: *s
 */
 char *string_toupper(char *s)
