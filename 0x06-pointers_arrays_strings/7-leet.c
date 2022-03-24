2e098e7d71df40aea5086484ffd0f46a59c716cb
Ima <98466210+Masterpiece513@users.noreply.github.com>
2022-03-24 13:37:17 +0100

    encode a string

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
-*leet - function that encodes a string
-*@str: input string
-*Return: encoded string
-*/
+ * leet - encodes a string
+ * @str: input String
+ * Return: encoded string
+ */
 char *leet(char *str)
 {
 	int index1 = 0, index2;
@@ -14,7 +14,9 @@ char *leet(char *str)
 		for (index2 = 0; index2 <= 7; index2++)
 		{
 			if (str[index1] == leet[index2] || str[index1] - 32 == leet[index2])
+			{
 				str[index1] = index2 + '0';
+			}
 		}
 	}
 	return (str);
