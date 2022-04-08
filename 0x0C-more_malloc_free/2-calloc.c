4ea2a46df319df71b716600566b5bc69c0ff5f16
Ima <98466210+Masterpiece513@users.noreply.github.com>
2022-04-08 19:03:06 +0100

    copy char

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,1 +1,18 @@
 #include "main.h"
+/**
+ * _memset - copy char
+ * @s: string
+ * @b: input
+ * @n: bytes
+ * Return: string
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
